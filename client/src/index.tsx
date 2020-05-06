e13e9ba4ae0d469302d0d6699184e848df185f94
SotaSuzuki <3.3sota.suzuki@gmail.com>
2020-05-06 21:45:28 +0900

    until a chapter of localstate managing

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,20 +1,46 @@
 import { ApolloClient } from 'apollo-client';
 import { InMemoryCache, NormalizedCacheObject } from 'apollo-cache-inmemory';
 import { HttpLink } from 'apollo-link-http';
-import { ApolloProvider } from '@apollo/react-hooks';
+import { ApolloProvider, useQuery } from '@apollo/react-hooks';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Pages from './pages';
+import Login from './pages/login';
+import gql from 'graphql-tag';
 import GlobalStyle from './styles';
+import { resolvers, typeDefs } from './resolvers';
+
+const IS_LOGGED_IN = gql`
+  query IsUserLoggedIn {
+    isLoggedIn @client
+  }
+`;
+
+function IsLoggedIn() {
+  const { data } = useQuery(IS_LOGGED_IN);
+  return data.isLoggedIn ? <Pages /> : <Login />;
+}
 
 const cache = new InMemoryCache();
 const link = new HttpLink({
   uri: 'http://localhost:4000',
+  headers: {
+    authorization: window.localStorage.getItem('token'),
+  },
 });
 
 const client: ApolloClient<NormalizedCacheObject> = new ApolloClient({
   cache,
   link,
+  typeDefs,
+  resolvers,
+});
+
+cache.writeData({
+  data: {
+    isLoggedIn: !!window.localStorage.getItem('token'),
+    cartItems: [],
+  },
 });
 
 ReactDOM.render(
--- client/src/pages/cart.tsx
+++ client/src/pages/cart.tsx
@@ -1,12 +1,43 @@
 import React from 'react';
+import { useQuery } from '@apollo/react-hooks';
+import gql from 'graphql-tag';
+import { Loading, Header } from '../components';
+import { BookTrips, CartItem } from '../containers';
 import { RouteComponentProps } from '@reach/router';
+import * as GetCartItemsTypes from './__generated__/GetCartItems';
 
-interface CartProps extends RouteComponentProps {
+export const GET_CART_ITEMS = gql`
+  query GetCartItems {
+    cartItems @client
+  }
+`;
 
-}
+interface CartProps extends RouteComponentProps {}
 
 const Cart: React.FC<CartProps> = () => {
-  return <div />;
-}
+  const { data, loading, error } = useQuery<GetCartItemsTypes.GetCartItems>(
+    GET_CART_ITEMS
+  );
+
+  if (loading) return <Loading />;
+  if (error) return <p>ERROR: {error.message}</p>;
+
+  return (
+    <React.Fragment>
+      <Header>My Cart</Header>
+      {!data || (!!data && data.cartItems.length === 0) ? (
+        <p data-testid="empty-message">No items in your cart</p>
+      ) : (
+        <React.Fragment>
+          {!!data &&
+            data.cartItems.map((launchId) => (
+              <CartItem key={launchId} launchId={launchId} />
+            ))}
+          <BookTrips cartItems={!!data ? data.cartItems : []} />
+        </React.Fragment>
+      )}
+    </React.Fragment>
+  );
+};
 
 export default Cart;
